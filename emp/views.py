7a877485669f3695aa012c88f7f5a613302bc24c
User FOUND <tanishqmeshram99@gmail.com>
2024-04-22 21:14:22 +0530

    updated code v1.1

--- emp/views.py
+++ emp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+from emp.models import Emp
 
 # Create your views here.
+
+def employee_detail(request, pk):
+    employee = get_object_or_404(Emp, pk=pk)
+    return HttpResponse(employee)
--- myproj/views.py
+++ myproj/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from emp.models import Emp
 
 def home(request):
-    return render(request, 'home.html')
+    employees = Emp.objects.all()
+    context = {
+        'employees': employees,
+    }
+    return render(request, 'home.html', context)
